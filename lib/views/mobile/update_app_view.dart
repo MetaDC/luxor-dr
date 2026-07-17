4e5b403340be1390318bf668717259eb5caef05b
MetaDC <teammetadc@gmail.com>
2026-07-17 11:02:42 +0530

    zaid

--- lib/views/mobile/update_app_view.dart
+++ lib/views/mobile/update_app_view.dart
@@ -1,16 +1,29 @@
+import 'dart:io' as io;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
+import '../../controllers/auth_ctrl.dart';
 import '../../utils/app_theme.dart';
 
 class UpdateAppView extends StatelessWidget {
   const UpdateAppView({super.key});
 
   Future<void> _openStore() async {
-    final url = Uri.parse(
-      'https://play.google.com/store/apps/details?id=com.diwizon.luxor_dr',
-    );
+    Uri url;
+    if (io.Platform.isAndroid) {
+      url = Uri.parse(
+        'https://play.google.com/store/apps/details?id=com.diwizon.luxor_dr',
+      );
+    } else if (io.Platform.isIOS) {
+      // TODO: Replace 'YOUR_APP_ID' with the actual App Store ID when published
+      url = Uri.parse(
+        'https://apps.apple.com/us/app/luxor-admin/id6786658379',
+        // 'https://apps.apple.com/app/luxor-dr/idYOUR_APP_ID',
+      );
+    } else {
+      return;
+    }
+
     if (await canLaunchUrl(url)) {
       await launchUrl(url, mode: LaunchMode.externalApplication);
     }
@@ -80,6 +93,25 @@ class UpdateAppView extends StatelessWidget {
                           fontSize: 15,
                           fontWeight: FontWeight.w600,
                         ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextButton.icon(
+                    onPressed: () async {
+                      await AuthCtrl.to.signOut();
+                    },
+                    icon: const Icon(
+                      Icons.logout_rounded,
+                      color: DrColors.error,
+                      size: 20,
+                    ),
+                    label: Text(
+                      'Logout',
+                      style: GoogleFonts.inter(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                        color: DrColors.error,
                       ),
                     ),
                   ),
